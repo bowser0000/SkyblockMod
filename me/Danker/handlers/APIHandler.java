0f36b9862ce9d49c45e4bb53a913a37477181992
bowser0000 <bowser0000@gmail.com>
2020-07-23 22:11:59 -0400

    Fix last_save profile values
    Sometimes the last_save would rollover into the negatives, and would return null profile.

--- me/Danker/handlers/APIHandler.java
+++ me/Danker/handlers/APIHandler.java
@@ -120,12 +120,12 @@ public class APIHandler {
 		// Loop through profiles to find latest
 		System.out.println("Looping through profiles...");
 		String latestProfile = "";
-		int latestSave = 0;
+		long latestSave = 0;
 		JsonArray profilesArray = profilesResponse.get("profiles").getAsJsonArray();
 		
 		for (JsonElement profile : profilesArray) {
 			JsonObject profileJSON = profile.getAsJsonObject();
-			int profileLastSave = profileJSON.get("members").getAsJsonObject().get(UUID).getAsJsonObject().get("last_save").getAsInt();
+			long profileLastSave = profileJSON.get("members").getAsJsonObject().get(UUID).getAsJsonObject().get("last_save").getAsLong();
 			
 			if (profileLastSave > latestSave) {
 				latestProfile = profileJSON.get("profile_id").getAsString();
