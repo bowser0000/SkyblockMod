a29cfb42db5da544f8445f56acb3742e396d8dee
bowser0000 <bowser0000@gmail.com>
2020-09-05 18:34:03 -0400

    Change version to 1.7

--- src/main/java/me/Danker/TheMod.java
+++ src/main/java/me/Danker/TheMod.java
@@ -78,7 +78,7 @@ import net.minecraftforge.fml.common.versioning.DefaultArtifactVersion;
 public class TheMod
 {
     public static final String MODID = "Danker's Skyblock Mod";
-    public static final String VERSION = "1.6.2";
+    public static final String VERSION = "1.7";
     
     static double checkItemsNow = 0;
     static double itemsChecked = 0;
