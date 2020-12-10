0c8bda1714891bb26979f723f40db299896b1cce
bowser0000 <bowser0000@gmail.com>
2020-12-09 20:47:16 -0500

    Add debug command to /dsm
    Copies important information to clipboard to help with debugging issues

--- src/main/java/me/Danker/DankersSkyblockMod.java
+++ src/main/java/me/Danker/DankersSkyblockMod.java
@@ -73,7 +73,7 @@ import java.util.regex.Pattern;
 public class DankersSkyblockMod
 {
     public static final String MODID = "Danker's Skyblock Mod";
-    public static final String VERSION = "1.8.4";
+    public static final String VERSION = "1.8.5-beta1";
     
     static double checkItemsNow = 0;
     static double itemsChecked = 0;
