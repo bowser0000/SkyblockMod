f3009ab52d8b70af31ac3e1ba768f7bba852f485
Kal Draganov <kaldraganov05@gmail.com>
2020-12-11 17:41:44 -0500

    Update version

--- src/main/java/me/Danker/DankersSkyblockMod.java
+++ src/main/java/me/Danker/DankersSkyblockMod.java
@@ -74,7 +74,7 @@ import java.util.regex.Pattern;
 public class DankersSkyblockMod
 {
     public static final String MODID = "Danker's Skyblock Mod";
-    public static final String VERSION = "1.8.5-beta1";
+    public static final String VERSION = "1.8.5-beta2";
     
     static double checkItemsNow = 0;
     static double itemsChecked = 0;
