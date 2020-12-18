6baa9f89600d23e0b7265b0a30d6003b928bf4a0
Sychic <Sychic@github.com>
2020-12-17 21:52:20 -0500

    update version

--- src/main/java/me/Danker/DankersSkyblockMod.java
+++ src/main/java/me/Danker/DankersSkyblockMod.java
@@ -79,7 +79,7 @@ import java.util.regex.Pattern;
 public class DankersSkyblockMod
 {
     public static final String MODID = "Danker's Skyblock Mod";
-    public static final String VERSION = "1.8.5-beta5";
+    public static final String VERSION = "1.8.5-beta6";
 
     static double checkItemsNow = 0;
     static double itemsChecked = 0;
