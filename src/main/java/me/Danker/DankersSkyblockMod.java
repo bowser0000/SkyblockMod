6c51a4c8aa217ff1ba50c1ac251fd38aef58a0cb
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2020-12-22 17:50:28 -0500

    Beta 6

--- src/main/java/me/Danker/DankersSkyblockMod.java
+++ src/main/java/me/Danker/DankersSkyblockMod.java
@@ -78,7 +78,7 @@ import java.util.regex.Pattern;
 @Mod(modid = DankersSkyblockMod.MODID, version = DankersSkyblockMod.VERSION, clientSideOnly = true)
 public class DankersSkyblockMod {
     public static final String MODID = "Danker's Skyblock Mod";
-    public static final String VERSION = "1.8.5-beta5";
+    public static final String VERSION = "1.8.5-beta6";
 
     static double checkItemsNow = 0;
     static double itemsChecked = 0;
