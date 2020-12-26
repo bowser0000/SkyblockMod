3f2314a67c372438f04f4c71e5bb22f2cdc8f683
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2020-12-26 15:29:33 -0500

    Add Mixins to client launch

---
+++ src/main/java/me/Danker/tweaker/DSMLoadingPlugin.java
@@ -0,0 +1,59 @@
+package me.Danker.tweaker;
+
+import net.minecraftforge.fml.relauncher.CoreModManager;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.apache.logging.log4j.LogManager;
+import org.spongepowered.asm.launch.MixinBootstrap;
+
+import javax.annotation.Nullable;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.security.CodeSource;
+import java.util.Map;
+
+public class DSMLoadingPlugin implements IFMLLoadingPlugin {
+
+    public DSMLoadingPlugin() {
+        MixinBootstrap.init();
+
+        CodeSource codeSource = this.getClass().getProtectionDomain().getCodeSource();
+        if (codeSource != null) {
+            URL location = codeSource.getLocation();
+            try {
+                File file = new File(location.toURI());
+                if (file.isFile()) {
+                    CoreModManager.getReparseableCoremods().remove(file.getName());
+                }
+            } catch (URISyntaxException ignored) {}
+        } else {
+            LogManager.getLogger().warn("No CodeSource, if this is not a development environment we might run into problems!");
+            LogManager.getLogger().warn(this.getClass().getProtectionDomain());
+        }
+    }
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[0];
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+}
