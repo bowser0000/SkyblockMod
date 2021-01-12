9838bc712ff8c4614a658b1bde4dae913f03541f
Sychic <Sychic@github.com>
2021-01-11 21:51:20 -0500

    Revert reparty edit commits 2/2

--- src/main/java/me/Danker/DankersSkyblockMod.java
+++ src/main/java/me/Danker/DankersSkyblockMod.java
@@ -63,7 +63,6 @@ import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
 import net.minecraftforge.fml.common.network.FMLNetworkEvent.ClientConnectedToServerEvent;
 import net.minecraftforge.fml.common.versioning.DefaultArtifactVersion;
 import org.apache.commons.lang3.time.StopWatch;
-import org.apache.logging.log4j.Logger;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.GL11;
@@ -80,7 +79,6 @@ import java.util.regex.Pattern;
 public class DankersSkyblockMod {
     public static final String MODID = "Danker's Skyblock Mod";
     public static final String VERSION = "1.8.5-beta8";
-    public static Logger DSMLogger;
     static double checkItemsNow = 0;
     static double itemsChecked = 0;
     public static Map<String, String> t6Enchants = new HashMap<>();
@@ -110,9 +108,8 @@ public class DankersSkyblockMod {
 
     public static final ResourceLocation CAKE_ICON = new ResourceLocation("dsm", "icons/cake.png");
     public static final ResourceLocation BONZO_ICON = new ResourceLocation("dsm", "icons/bonzo.png");
-	public static final ResourceLocation LOGO = new ResourceLocation("dsm", "icons/dsm.png");
 
-	static String[] riddleSolutions = {"The reward is not in my chest!", "At least one of them is lying, and the reward is not in",
+    static String[] riddleSolutions = {"The reward is not in my chest!", "At least one of them is lying, and the reward is not in",
             "My chest doesn't have the reward. We are all telling the truth", "My chest has the reward and I'm telling the truth",
             "The reward isn't in any of our chests", "Both of them are telling the truth."};
     static Map<String, String[]> triviaSolutions = new HashMap<>();
@@ -288,7 +285,6 @@ public class DankersSkyblockMod {
 
     @EventHandler
     public void preInit(final FMLPreInitializationEvent event) {
-        DSMLogger = event.getModLog();
     	ClientCommandHandler.instance.registerCommand(new ToggleCommand());
     	ClientCommandHandler.instance.registerCommand(new SetkeyCommand());
     	ClientCommandHandler.instance.registerCommand(new GetkeyCommand());
@@ -319,7 +315,6 @@ public class DankersSkyblockMod {
     public void postInit(final FMLPostInitializationEvent event) {
 		Package[] packages = Package.getPackages();
 		for(Package p : packages){
-		    DSMLogger.info(p);
 			if(p.getName().startsWith("com.spiderfrog.gadgets") || p.getName().startsWith("com.spiderfrog.oldanimations")){
 				usingOAM = true;
 				break;
@@ -431,125 +426,38 @@ public class DankersSkyblockMod {
         }
 
 		// Reparty command
-        // Getting party
-        if (RepartyCommand.gettingParty) {
-            if (message.contains("-----")) {
-                switch(RepartyCommand.Delimiter) {
-                    case 0:
-                        System.out.println("Get Party Delimiter Cancelled");
-                        RepartyCommand.Delimiter++;
-                        event.setCanceled(true);
-                        return;
-                    case 1:
-                        System.out.println("Done querying party");
-                        RepartyCommand.gettingParty = false;
-                        RepartyCommand.Delimiter = 0;
-                        event.setCanceled(true);
-                        return;
-                }
-            }else if (message.startsWith("Party M") || message.startsWith("Party Leader")){
-                EntityPlayerSP player = Minecraft.getMinecraft().thePlayer;
-
-                Pattern party_start_pattern = Pattern.compile("^Party Members \\((\\d+)\\)$");
-                Pattern leader_pattern = Pattern.compile("^Party Leader: (?:\\[.+?] )?(\\w+) ●$");
-                Pattern members_pattern = Pattern.compile(" (?:\\[.+?] )?(\\w+) ●");
-                Matcher party_start = party_start_pattern.matcher(message);
-                Matcher leader = leader_pattern.matcher(message);
-                Matcher members = members_pattern.matcher(message);
-
-                if (party_start.matches() && Integer.parseInt(party_start.group(1)) == 1) {
-                    player.addChatMessage(new ChatComponentText(DankersSkyblockMod.ERROR_COLOUR + "You cannot reparty yourself."));
-                    RepartyCommand.partyThread.interrupt();
-                } else if (leader.matches() && !(leader.group(1).equals(player.getName()))) {
-                    player.addChatMessage(new ChatComponentText(DankersSkyblockMod.ERROR_COLOUR + "You are not party leader."));
-                    RepartyCommand.partyThread.interrupt();
-                } else {
-                    while (members.find()) {
-                        String partyMember = members.group(1);
-                        if (!partyMember.equals(player.getName())) {
-                            RepartyCommand.party.add(partyMember);
-                            System.out.println(partyMember);
-                        }
-                    }
-                }
-                event.setCanceled(true);
-                return;
-            }
-        }
-        // Disbanding party
-        if (RepartyCommand.disbanding) {
-            if (message.contains("-----")) {
-                switch (RepartyCommand.Delimiter) {
-                    case 0:
-                        System.out.println("Disband Delimiter Cancelled");
-                        RepartyCommand.Delimiter++;
-                        event.setCanceled(true);
-                        return;
-                    case 1:
-                        System.out.println("Done disbanding");
-                        RepartyCommand.disbanding = false;
-                        RepartyCommand.Delimiter = 0;
-                        event.setCanceled(true);
-                        return;
-                }
-            } else if (message.endsWith("has disbanded the party!")) {
-                event.setCanceled(true);
-                return;
-            }
-        }
-        // Inviting
-        if (RepartyCommand.inviting) {
-            if (message.contains("-----")) {
-                if (RepartyCommand.Delimiter >= RepartyCommand.party.size() * 2 - 1) {
-                    event.setCanceled(true);
-                    RepartyCommand.Delimiter = 0;
-                    System.out.println("Done Inviting!");
-                    RepartyCommand.inviting = false;
-                    return;
-                } else {
-                    RepartyCommand.Delimiter++;
-                    event.setCanceled(true);
-                    return;
-                }
-            } else if (message.endsWith(" to the party! They have 60 seconds to accept.")) {
-                Pattern invitePattern = Pattern.compile("(?:(?:\\[.+?] )?(?:\\w+) invited )(?:\\[.+?] )?(\\w+)");
-                Matcher invitee = invitePattern.matcher(message);
-                if (invitee.find()) {
-                    System.out.println("" + invitee.group(1) + ": " + RepartyCommand.repartyFailList.remove(invitee.group(1)));
-                }
-                event.setCanceled(true);
-                return;
-            } else if (message.contains("Couldn't find a player") || message.contains("You cannot invite that player")) {
-                event.setCanceled(true);
-                return;
-            }
-        }
-        // Fail Inviting
-        if (RepartyCommand.failInviting) {
-            if (message.contains("-----")) {
-                if (RepartyCommand.Delimiter >= RepartyCommand.repartyFailList.size() * 2 - 1) {
-                    event.setCanceled(true);
-                    RepartyCommand.Delimiter = 0;
-                    System.out.println("Done Inviting!");
-                    RepartyCommand.inviting = false;
-                    return;
-                } else {
-                    RepartyCommand.Delimiter++;
-                    event.setCanceled(true);
-                    return;
-                }
-            } else if (message.endsWith(" to the party! They have 60 seconds to accept.")) {
-                Pattern invitePattern = Pattern.compile("(?:(?:\\[.+?] )?(?:\\w+) invited )(?:\\[.+?] )?(\\w+)");
-                Matcher invitee = invitePattern.matcher(message);
-                if (invitee.find()) {
-                    System.out.println("" + invitee.group(1) + ": " + RepartyCommand.repartyFailList.remove(invitee.group(1)));
-                }
-                event.setCanceled(true);
-                return;
-            } else if (message.contains("Couldn't find a player") || message.contains("You cannot invite that player")) {
-                event.setCanceled(true);
-                return;
-            }
+		if (System.currentTimeMillis() / 1000 - RepartyCommand.callTime <= 5) {
+			if (!(message.contains("----") || message.contains("disbanded") || message.contains("seconds to accept") || message.contains("●") || message.contains("Party Members") || message.contains("Couldn't find a player") || message.contains("cannot invite that player") || message.length() == 0)) {
+				return;
+			}
+			
+			EntityPlayerSP player = Minecraft.getMinecraft().thePlayer;
+			
+			Pattern party_start_pattern = Pattern.compile("^Party Members \\((\\d+)\\)$");
+            Pattern leader_pattern = Pattern.compile("^Party Leader: (?:\\[.+?] )?(\\w+) ●$");
+			Pattern members_pattern = Pattern.compile(" (?:\\[.+?] )?(\\w+) ●");
+            Matcher party_start = party_start_pattern.matcher(message);
+            Matcher leader = leader_pattern.matcher(message);
+            Matcher members = members_pattern.matcher(message);
+			
+			if (party_start.matches() && Integer.parseInt(party_start.group(1)) == 1) {
+				player.addChatMessage(new ChatComponentText(DankersSkyblockMod.ERROR_COLOUR + "You cannot reparty yourself."));
+			}
+            else if (leader.matches() && !(leader.group(1).equals(player.getName()))) {
+				player.addChatMessage(new ChatComponentText(DankersSkyblockMod.ERROR_COLOUR + "You are not party leader."));
+			}
+			else if (message.contains("Couldn't find a player") || message.contains("You cannot invite that player")) {
+				RepartyCommand.repartyFailList.add(RepartyCommand.currentMember);
+			}
+			else {
+				while (members.find()) {
+					String partyMember = members.group(1);
+					if (!partyMember.equals(player.getName())) {
+						RepartyCommand.party.add(partyMember);
+					}
+				}
+			}
+			event.setCanceled(true);
         }
 
     	if (!Utils.inSkyblock) return;
@@ -781,7 +689,7 @@ public class DankersSkyblockMod {
                 return;
             }
         }
-
+  
         if (ToggleCommand.oruoToggled && Utils.inDungeons) {
         	if (message.contains("What SkyBlock year is it?")) {
                 double currentTime = System.currentTimeMillis() /1000L;
