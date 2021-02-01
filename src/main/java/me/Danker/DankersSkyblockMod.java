87b5221d534cbb9171c4bbbf76fc5a190d3d24ef
Quantizr <jimmydfang@gmail.com>
2021-01-31 23:36:32 -0800

    Fix monolith locator crashing outside of Dwarven Mines

--- src/main/java/me/Danker/DankersSkyblockMod.java
+++ src/main/java/me/Danker/DankersSkyblockMod.java
@@ -3117,7 +3117,8 @@ public class DankersSkyblockMod {
 
     @SubscribeEvent
     public void onWorldRender(RenderWorldLastEvent event) {
-        if (ToggleCommand.monolithWaypointsToggled && LocationHandler.getLocation().equals("mining_3")) {
+        if (ToggleCommand.monolithWaypointsToggled && LocationHandler.getLocation() != null) {
+            if (!LocationHandler.getLocation().equals("mining_3")) return;
             DarkMonolithUtils.drawWaypoint(DarkMonolithUtils.findMonolith(), event.partialTicks);
         }
         if (ToggleCommand.blazeToggled) {
