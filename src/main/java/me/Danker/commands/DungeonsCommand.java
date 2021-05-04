8cf11c262ec7c2ba89365671c1434799f862faa7
bowser0000 <bowser0000@gmail.com>
2021-05-04 17:07:41 -0400

    Add master mode completions to /dungeons

--- src/main/java/me/Danker/commands/DungeonsCommand.java
+++ src/main/java/me/Danker/commands/DungeonsCommand.java
@@ -86,9 +86,10 @@ public class DungeonsCommand extends CommandBase {
 			String playerURL = "https://api.hypixel.net/player?uuid=" + uuid + "&key=" + key;
 			System.out.println("Fetching player data...");
 			JsonObject playerResponse = APIHandler.getResponse(playerURL);
-			if(!playerResponse.get("success").getAsBoolean()){
+			if (!playerResponse.get("success").getAsBoolean()) {
 				String reason = playerResponse.get("cause").getAsString();
-				player.addChatMessage(new ChatComponentText(DankersSkyblockMod.ERROR_COLOUR + "This player has not played on Hypixel."));
+				player.addChatMessage(new ChatComponentText(DankersSkyblockMod.ERROR_COLOUR + "Failed with reason: " + reason));
+				return;
 			}
 			
 			System.out.println("Fetching dungeon stats...");
@@ -111,33 +112,56 @@ public class DungeonsCommand extends CommandBase {
 			int highestFloor = catacombsObject.get("highest_tier_completed").getAsInt();
 			JsonObject completionObj = catacombsObject.get("tier_completions").getAsJsonObject();
 
-			String delimiter = DankersSkyblockMod.DELIMITER_COLOUR + "" + EnumChatFormatting.BOLD + "-------------------";
+			JsonObject catacombsMasterObject = dungeonsObject.get("dungeon_types").getAsJsonObject().get("master_catacombs").getAsJsonObject();
+			boolean hasPlayedMaster = catacombsMasterObject.has("highest_tier_completed");
 
-			ChatComponentText classLevels = new ChatComponentText(
-					EnumChatFormatting.GOLD + " Selected Class: " + selectedClass + "\n\n" +
-						 EnumChatFormatting.RED + " Catacombs Level: " + catacombs + "\n" +
-						 EnumChatFormatting.YELLOW + " Healer Level: " + healer + "\n" +
-						 EnumChatFormatting.LIGHT_PURPLE + " Mage Level: " + mage + "\n" +
-						 EnumChatFormatting.RED + " Berserk Level: " + berserk + "\n" +
-						 EnumChatFormatting.GREEN + " Archer Level: " + archer + "\n" +
-						 EnumChatFormatting.BLUE + " Tank Level: " + tank + "\n\n" +
-						 EnumChatFormatting.WHITE + " Secrets Found: " + secrets + "\n\n");
-
-			StringBuilder completionsHoverString = new StringBuilder();
-
-			for (int i = 0; i <= highestFloor; i++) {
-				completionsHoverString
-						.append(EnumChatFormatting.GOLD)
-						.append(i == 0 ? "Entrance: " : "Floor " + i + ": ")
-						.append(EnumChatFormatting.RESET)
-						.append(completionObj.get(String.valueOf(i)).getAsInt())
-						.append(i < highestFloor ? "\n": "");
+			int highestMasterFloor = 0;
+			JsonObject completionMasterObj = null;
+			if (hasPlayedMaster) {
+				highestMasterFloor = catacombsMasterObject.get("highest_tier_completed").getAsInt();
+				completionMasterObj = catacombsMasterObject.get("tier_completions").getAsJsonObject();
 			}
 
-			ChatComponentText completions = new ChatComponentText(EnumChatFormatting.GOLD + " Highest Floor Completed: " + highestFloor);
+			String delimiter = DankersSkyblockMod.DELIMITER_COLOUR + "" + EnumChatFormatting.BOLD + "-------------------";
 
+			ChatComponentText classLevels = new ChatComponentText(EnumChatFormatting.GOLD + " Selected Class: " + selectedClass + "\n\n" +
+																  EnumChatFormatting.RED + " Catacombs Level: " + catacombs + "\n" +
+																  EnumChatFormatting.YELLOW + " Healer Level: " + healer + "\n" +
+																  EnumChatFormatting.LIGHT_PURPLE + " Mage Level: " + mage + "\n" +
+																  EnumChatFormatting.RED + " Berserk Level: " + berserk + "\n" +
+																  EnumChatFormatting.GREEN + " Archer Level: " + archer + "\n" +
+																  EnumChatFormatting.BLUE + " Tank Level: " + tank + "\n\n" +
+																  EnumChatFormatting.WHITE + " Secrets Found: " + secrets + "\n\n");
+
+			StringBuilder completionsHoverString = new StringBuilder();
+			for (int i = 0; i <= highestFloor; i++) {
+				completionsHoverString
+					.append(EnumChatFormatting.GOLD)
+					.append(i == 0 ? "Entrance: " : "Floor " + i + ": ")
+					.append(EnumChatFormatting.RESET)
+					.append(completionObj.get(String.valueOf(i)).getAsInt())
+					.append(i < highestFloor || hasPlayedMaster ? "\n": "");
+			}
+			for (int i = 1; i <= highestMasterFloor; i++) {
+				if (completionMasterObj != null && completionMasterObj.has(String.valueOf(i))) {
+					completionsHoverString
+						.append(EnumChatFormatting.GOLD)
+						.append("Master Floor ")
+						.append(i)
+						.append(": ")
+						.append(EnumChatFormatting.RESET)
+						.append(completionMasterObj.get(String.valueOf(i)).getAsInt())
+						.append(i < highestMasterFloor ? "\n": "");
+				}
+			}
+
+			ChatComponentText completions;
+			if (hasPlayedMaster) {
+				completions = new ChatComponentText(EnumChatFormatting.GOLD + " Highest Floor Completed: Master " + highestMasterFloor);
+			} else {
+				completions = new ChatComponentText(EnumChatFormatting.GOLD + " Highest Floor Completed: " + highestFloor);
+			}
 			completions.setChatStyle(completions.getChatStyle().setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ChatComponentText(completionsHoverString.toString()))));
-
 
 			player.addChatMessage(
 					new ChatComponentText(delimiter)
@@ -145,8 +169,7 @@ public class DungeonsCommand extends CommandBase {
 					.appendSibling(classLevels)
 					.appendSibling(completions)
 					.appendText("\n")
-					.appendSibling(new ChatComponentText(delimiter))
-				);
+					.appendSibling(new ChatComponentText(delimiter)));
 		}).start();
 	}
 }
